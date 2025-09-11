23b218e0fe74a7ac57e6a279a02796b224fd2271
DarimarFlores <darimar.flores1984@gmail.com>
2025-09-11 14:32:23 -0300

    versao3

--- financas/views.py
+++ financas/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from financas.forms import RendaMensalForm
+from financas.models import RendaMensal
+
 
 # Create your views here.
+def Exibicacao(request):
+
+    if request.method == 'GET':
+        financas = RendaMensal.objects.all()
+        context = {
+            'RendaMensal': RendaMensal,
+            'form': RendaMensalForm
+        }
+        return render(request,'principal/index.html',context)
+    
+    if request.method == 'POST':
+        form = RendaMensalForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'principal/index.html')
